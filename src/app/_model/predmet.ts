da64e5328ff8d78958be4e3b318f0ef85ce69591
Vuckovic-Djordje <vuckovic.sf26.2019@uns.ac.rs>
2022-06-05 00:00:49 +0200

    Doradjivanje fronta

---
+++ src/app/_model/predmet.ts
@@ -0,0 +1,28 @@
+export class Predmet implements PredmetInterface{
+	public predmet_id: number;
+	public naziv: string;
+    public semestar: string;
+	public espb: string;
+	public opis: string;
+    
+		
+	constructor(predmetCfg:PredmetInterface)
+	{	
+		this.predmet_id = predmetCfg.predmet_id;
+		this.naziv = predmetCfg.naziv;
+        this.semestar = predmetCfg.semestar;
+		this.espb = predmetCfg.espb;
+		this.opis = predmetCfg.opis;
+        
+	}
+}
+
+interface PredmetInterface{
+    predmet_id: number;
+	naziv: string;
+    semestar: string;
+	espb: string;
+	opis: string;
+	
+
+}
